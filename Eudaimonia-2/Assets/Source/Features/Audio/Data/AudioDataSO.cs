3c52b7f5955bf5dcbf849b8f4a5fc151ac43d002
Laska-xxx <ZubavichusEYa23@st.ithub.ru>
2026-05-27 16:02:34 +0300

    Audio data so

---
+++ Eudaimonia-2/Assets/Source/Features/Audio/Data/AudioDataSO.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioData", menuName = "Scriptable Objects/AudioData")]
+public class AudioDataSO : ScriptableObject
+{
+    public AudioClip[] clips;
+    [Range(0f, 1f)] public float volume = 1f;
+    [Range(0.1f, 3f)] public float minPitch = 0.9f;
+    [Range(0.1f, 3f)] public float maxPitch = 1.1f;
+
+    public void Play(AudioSource source)
+    {
+        if (clips == null || clips.Length == 0) return;
+
+        source.clip = clips[Random.Range(0, clips.Length)];
+        source.volume = volume;
+        source.pitch = Random.Range(minPitch, maxPitch);
+        source.Play();
+    }
+}
